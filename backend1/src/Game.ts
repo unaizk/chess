e6e0585945187b47db52477ac2a924fdf2f407cc
root <root@Unaislaptop>
2024-05-11 14:51:08 +0530

    added the game class logic

---
+++ backend1/src/Game.ts
@@ -0,0 +1,17 @@
+import { WebSocket } from "ws";
+
+export class Game {
+    private player1 : WebSocket
+    private player2 : WebSocket
+    private moves : string[];
+    private board : string;
+    private startTime : Date
+    constructor (player1 : WebSocket, player2 : WebSocket){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = "";
+        this.moves = [];
+        this.startTime = new Date()
+
+    }
+}
